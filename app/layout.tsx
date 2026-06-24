46dfddf28314c3d8e23bf90a9a870ab2c463e7e8
Mr.Quoc Nguyen <quocnguyen753.pa@gmail.com>
2026-06-24 11:56:35 +0700

    feat: update metadata for localized quiz application

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Quizy - Test Your Knowledge!",
-  description: "A Quiz App built using Next JS",
+  title: "Quiz - Ôn tập 600 câi hỏi giấy phép lái xe",
+  description: "Ứng dụng web phục vụ cho việc ôn tập các câu hỏi thi lấy giấy phép lái xe",
 };
 
 export default function RootLayout({
